94d75ed4d02d4d15c7cff6bd1a0a48241516e686
ragnarok <angelriera1796@gmail.com>
2023-07-14 23:08:45 -0400

    first commit

---
+++ src/pages/backend/index.js
@@ -0,0 +1,8 @@
+
+
+export default function api(req, res) {
+
+
+    res.status(200).json( "api de backend" )
+  }
+  
---
+++ src/pages/page2.jsx
@@ -0,0 +1,7 @@
+
+
+export default function(){
+    return (
+        <h1>hola mundo esta es un client component vanilla next</h1>
+    )
+}
