d893b3154310c6fa02a76742bfb2fd8a298174f0
ragnarok <angelriera1796@gmail.com>
2023-07-23 14:42:30 -0400

    update for deploy

--- src/app/AddNew.jsx
+++ src/app/AddNew.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 
-export default function () {
+export default function AddNew() {
 
     const [visible, setVisible] = useState()
     const [data, setData] = useState({
